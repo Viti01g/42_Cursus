5ebab4eea6d8e4abae4c39ae30fc370fdfd41293
Viti01g <60265664+Viti01g@users.noreply.github.com>
2023-08-05 20:22:03 +0200

    Libft / so_long

---
+++ so_long/libft/get_next_line_utils.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vruiz-go <vruiz-go@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 13:22:40 by vruiz-go          #+#    #+#             */
+/*   Updated: 2023/07/16 16:33:40 by vruiz-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_substr_gnl(const char *s, unsigned int start, size_t len)
+{
+	char	*aux;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (ft_strlen_(s) <= start)
+		return (0);
+	if (ft_strlen_(&s[start]) < len)
+		len = ft_strlen_(&s[start]);
+	aux = (char *)malloc(len + 1);
+	if (!aux)
+		return (0);
+	while (s[start] != 0 && i < len)
+	aux[i++] = s[start++];
+	aux[i] = 0;
+	return (aux);
+}
+
+size_t	ft_strlen_(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr_gnl(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (str[i] == (char)c)
+		return ((char *)&str[i]);
+	return (NULL);
+}
+
+char	*ft_strdup_gnl(const char *s)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = ft_strlen_(s);
+	str = (char *)malloc(sizeof(*str) * (j + 1));
+	if (!str)
+		return (NULL);
+	while (i < j)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoini_gnl(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL && s2 != NULL)
+		return (ft_strdup_gnl(s2));
+	str = (char *)malloc(sizeof(char) * (ft_strlen_(s1) + ft_strlen_(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
